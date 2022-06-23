415f05f7f0027d44b8cea413e279676e4f0fd24a
matthew <lordlance>
2022-06-23 15:13:08 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <h2>Reaction Timer</h2>
+  <button @click="start" :disabled="isPlaying">play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+  <Result v-if="showResults" :score="score"/>
+</template>
+
+<script>
+
+import Block from './components/Block.vue'
+import Result from './components/Result.vue'
+
+export default {
+  name: 'App',
+  components: { Block , Result},
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false
+    }
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000 ,
+      this.isPlaying = true
+      this.showResults = false
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResults = true
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+button{
+  background: green;
+  color: white;
+  width: 50px;
+  height: 24px;
+  border-radius: 4px;
+}
+button:hover{
+  background: white;
+  color: grey;
+}
+</style>
---
+++ src/components/Result.vue
@@ -0,0 +1,32 @@
+<template>
+    <p>Reaction time - {{ score }}</p>
+    <p class="rank">{{ rank }}</p>
+</template>
+
+<script >
+    export default{
+        props: ['score'],
+        data() {
+            return{
+                rank: null
+            }
+        },
+        mounted() {
+            if(this.score < 250){
+                this.rank = 'Super-fast fingers'
+            }else if (this.score < 400) {
+                this.rank = 'fast fingers'
+            } else{
+                this.rank = 'slow fingers...'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .rank {
+        font-size: 1.4em;
+        color: #0faf87;
+        font-weight: bold;
+    }
+</style>
